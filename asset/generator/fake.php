41d12ba31f0ca2af6eb1d67fbd99d82063306d0c
Olivier Stahl <cortex2001@gmail.com>
2013-12-20 11:22:12 +0000

    First commit : * Home view done * Register view done

---
+++ asset/generator/fake.php
@@ -0,0 +1,38 @@
+<?php
+namespace Beable\Kernel\Asset\Generator;
+
+use Beable\Kernel;
+
+class Fake extends Kernel\Asset\Generator
+{
+	/**
+	 * @return int
+	 */
+	public function generateAsset()
+	{
+		$content = $this->minify($this->getFullContent());
+		$path = $this->getAsset()->getPath();
+		$dirPath = dirname($path);
+		if (!file_exists($dirPath)) {
+			mkdir($dirPath, 0755, true);
+		}
+		return file_put_contents($path, $content);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHtml()
+	{
+		return '<script type="text/javascript" src="' . $this->getAsset()->getUrl() . '"></script>';
+	}
+
+	/**
+	 * @param string $content
+	 * @return string
+	 */
+	public function minify($content)
+	{
+		return $content;
+	}
+}
