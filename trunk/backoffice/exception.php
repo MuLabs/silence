c38b0e53bc4e5c14332614e078c7b568400c73fd
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-01-14 10:04:15 +0000

    FWK-82 : Add simple version of widget management

--- trunk/backoffice/exception.php
+++ trunk/backoffice/exception.php
@@ -7,6 +7,7 @@ class Exception extends Kernel\Exception
 {
 	const SECTION_NOT_FOUND = 1;
 	const WIDGET_NOT_FOUND = 2;
+	const WIDGET_NOT_ALLOWED = 2;
 
 	/**
 	 * @return string
@@ -18,7 +19,10 @@ class Exception extends Kernel\Exception
 				return 'Backoffice section not found : ' . $this->message;
 				break;
 			case self::WIDGET_NOT_FOUND:
-				return 'Widget section not found : ' . $this->message;
+				return 'Widget not found : ' . $this->message;
+				break;
+			case self::WIDGET_NOT_ALLOWED:
+				return 'Widget are not allowed on this section : ' . $this->message;
 				break;
 			default:
 				return parent::getFormatedMessage();
