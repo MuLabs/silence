522df7d098cdcb55b6ff0a141aab111031ef13f7
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-01-23 14:50:31 +0000

    FWK-85 : Update done are now saved in DB FWK-90 : Bundle can now handle updates

--- trunk/bundle/core.php
+++ trunk/bundle/core.php
@@ -6,4 +6,14 @@ use Mu\Kernel;
 abstract class Core extends Kernel\Core
 {
 	abstract public function initialize();
+
+	abstract public function getMainPath();
+
+	/**
+	 * @return string
+	 */
+	public function getUpdatePath()
+	{
+		return $this->getMainPath() . '/update/';
+	}
 }
