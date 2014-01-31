1f44c61585ffdbdf3303a6bd33d7c7a3a8ee6067
squiaios <squiaios@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-01-31 17:18:58 +0000

    Fix 228 : Front Glices

--- trunk/view/twig/extension/core.php
+++ trunk/view/twig/extension/core.php
@@ -13,12 +13,13 @@ class Core extends \Twig_Extension
 	public function getFunctions()
 	{
 		return array(
-			'asset' => new \Twig_Function_Method($this, 'asset', array('is_safe' => array('html'))),
-			'getUrl' => new \Twig_Function_Method($this, 'getUrl'),
-			'getUrlStatic' => new \Twig_Function_Method($this, 'getUrlStatic'),
-			'thisUrl' => new \Twig_Function_Method($this, 'thisUrl'),
-			'addFragment' => new \Twig_Function_Method($this, 'addFragment', array('is_safe' => array('html'))),
-			'thisFragment' => new \Twig_Function_Method($this, 'thisFragment', array('is_safe' => array('html'))),
+			'asset' 		=> new \Twig_Function_Method($this, 'asset', array('is_safe' => array('html'))),
+			'getUrl' 		=> new \Twig_Function_Method($this, 'getUrl'),
+			'getUrlStatic' 	=> new \Twig_Function_Method($this, 'getUrlStatic'),
+			'thisUrl' 		=> new \Twig_Function_Method($this, 'thisUrl'),
+			'addFragment' 	=> new \Twig_Function_Method($this, 'addFragment', array('is_safe' => array('html'))),
+			'thisFragment' 	=> new \Twig_Function_Method($this, 'thisFragment', array('is_safe' => array('html'))),
+			'strftime' 		=> new \Twig_Filter_Function('strftime'),
 		);
 	}
 
