18b8a93dc20c30d9f624b9987a264b5b522ea449
Schiano Romain <rschiano@users.noreply.github.com>
2014-02-07 15:47:42 +0000

    Always use framework native routing

--- route/dumper/nginx.php
+++ route/dumper/nginx.php
@@ -13,11 +13,13 @@ class Nginx extends Kernel\Route\Dumper
 		$content = "location / {\n";
 		$content .= "\troot " . PUBLIC_PATH . ";\n";
 		$content .= "\tindex index.php;\n";
-		foreach ($routes as $route) {
+		/*foreach ($routes as $route) {
 			$infos = $this->prepareRuleVars($route);
 			$content .= "\trewrite " . $infos['pattern'] . ' ' . $infos['dest'] . " break;\n";
-		}
+		}*/
 
+		$content .= "\trewrite ^/favicon\\.ico$ /favicon.ico break;\n";
+		$content .= "\trewrite ^.*\\.?(html|json)?$ /index.php?format=$1 break;\n\n";
 		$content .= "\tfastcgi_pass   127.0.0.1:9001;\n";
 		$content .= "\tfastcgi_index  index.php;\n";
 		$content .= "\t" . 'fastcgi_param  SCRIPT_FILENAME   $document_root$fastcgi_script_name;' . "\n";
@@ -27,37 +29,37 @@ class Nginx extends Kernel\Route\Dumper
 		file_put_contents(PUBLIC_PATH . '/' . $this->getApp()->getName() . '.conf', $content);
 	}
 
-	/**
+	/*
 	 * @param Kernel\Route\Route $route
 	 * @return array
 	 */
-	public function prepareRuleVars(Kernel\Route\Route $route)
-	{
-		$pattern = $route->getPattern();
-		$pattern = str_replace('/', '\/', '^' . $pattern . '\.?(' . implode('|', $route->getAllowedFormats()) . ')?$');
-		$vars = array(
-			'rn=' . $route->getName()
-		);
+	/*	public function prepareRuleVars(Kernel\Route\Route $route)
+		{
+			$pattern = $route->getPattern();
+			$pattern = str_replace('/', '\/', '^' . $pattern . '\.?(' . implode('|', $route->getAllowedFormats()) . ')?$');
+			$vars = array(
+				'rn=' . $route->getName()
+			);
 
-		$defaultVars = $route->getDefaultVars();
-		while (preg_match('#\{([^\}\/]+)}#iu', $pattern, $matches)) {
-			$match = $matches[1];
+			$defaultVars = $route->getDefaultVars();
+			while (preg_match('#\{([^\}\/]+)}#iu', $pattern, $matches)) {
+				$match = $matches[1];
 
-			if (isset($defaultVars[$match])) {
-				$pattern = str_replace('{' . $match . '}\/', '([^\/\.]+)?\/?', $pattern);
+				if (isset($defaultVars[$match])) {
+					$pattern = str_replace('{' . $match . '}\/', '([^\/\.]+)?\/?', $pattern);
+				}
+				$pattern = str_replace('{' . $match . '}', '([^\/\.]+)', $pattern);
+				$vars[] = $match . '=$' . count($vars);
 			}
-			$pattern = str_replace('{' . $match . '}', '([^\/\.]+)', $pattern);
-			$vars[] = $match . '=$' . count($vars);
-		}
 
-		// Add format var:
-		$vars[] = 'format=$' . count($vars);
+			// Add format var:
+			$vars[] = 'format=$' . count($vars);
 
-		$dest = '/index.php?' . implode('&', $vars);
+			$dest = '/index.php?' . implode('&', $vars);
 
-		return array(
-			'dest' => $dest,
-			'pattern' => $pattern
-		);
-	}
+			return array(
+				'dest' => $dest,
+				'pattern' => $pattern
+			);
+		}*/
 }
