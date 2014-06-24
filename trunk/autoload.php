6759e02d90d319e42790f679c048fdabedc2fd27
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-06-24 12:39:25 +0000

    FWK-158 : Test WebSVN

--- trunk/autoload.php
+++ trunk/autoload.php
@@ -2,26 +2,26 @@
 
 function autoLoader($name)
 {
-	$path = '';
-	$prefix = substr($name, 0, strpos($name, '\\', strpos($name, '\\') + 1) + 1);
-	if ($prefix === 'Mu\\Kernel\\') {
-		$name = str_replace(array('Mu\\Kernel\\', '\\'), array('/', '/'), $name);
-		$path = KERNEL_PATH;
-	} elseif ($prefix === 'Mu\\App\\') {
-		$name = str_replace(array('Mu\\App\\', '\\'), array('/', '/'), $name);
-		$path = APP_PATH;
-	} elseif ($prefix === 'Mu\\Bundle\\') {
-		$name = str_replace(array('Mu\\Bundle\\', '\\'), array('/', '/'), $name);
-		$path = BUNDLE_PATH;
-	}
+    $path = '';
+    $prefix = substr($name, 0, strpos($name, '\\', strpos($name, '\\') + 1) + 1);
+    if ($prefix === 'Mu\\Kernel\\') {
+        $name = str_replace(array('Mu\\Kernel\\', '\\'), array('/', '/'), $name);
+        $path = KERNEL_PATH;
+    } elseif ($prefix === 'Mu\\App\\') {
+        $name = str_replace(array('Mu\\App\\', '\\'), array('/', '/'), $name);
+        $path = APP_PATH;
+    } elseif ($prefix === 'Mu\\Bundle\\') {
+        $name = str_replace(array('Mu\\Bundle\\', '\\'), array('/', '/'), $name);
+        $path = BUNDLE_PATH;
+    }
 
-	if ($path) {
-		$file = $path . '/' . strtolower($name) . '.php';
+    if ($path) {
+        $file = $path . '/' . strtolower($name) . '.php';
 
-		if (file_exists($file)) {
-			require($file);
-		}
-	}
+        if (file_exists($file)) {
+            require($file);
+        }
+    }
 }
 
 spl_autoload_register('autoLoader');
