2bc6d31430bc0eef9d8886b7c087485fd90fedc4
Schiano Romain <rschiano@users.noreply.github.com>
2014-07-24 14:28:02 +0000

    GLICES-175 : Auto select plateform if possible

--- toolbox.php
+++ toolbox.php
@@ -826,51 +826,6 @@ class Toolbox extends Service\Core
     }
 
     /**
-     * Get current OS name
-     * @return string
-     */
-    public function getOS()
-    {
-        $userAgent = $this->getApp()->getHttp()->getRequest()->getParameters(
-            'HTTP_USER_AGENT',
-            Kernel\Http\Request::PARAM_TYPE_SERVER,
-            ''
-        );
-        $testArray = array(
-            '/windows nt 6.2/i' => 'Windows 8',
-            '/windows nt 6.1/i' => 'Windows 7',
-            '/windows nt 6.0/i' => 'Windows Vista',
-            '/windows nt 5.2/i' => 'Windows Server 2003/XP x64',
-            '/windows nt 5.1/i' => 'Windows XP',
-            '/windows xp/i' => 'Windows XP',
-            '/windows nt 5.0/i' => 'Windows 2000',
-            '/windows me/i' => 'Windows ME',
-            '/win98/i' => 'Windows 98',
-            '/win95/i' => 'Windows 95',
-            '/win16/i' => 'Windows 3.11',
-            '/macintosh|mac os x/i' => 'Mac OS X',
-            '/mac_powerpc/i' => 'Mac OS 9',
-            '/linux/i' => 'Linux',
-            '/ubuntu/i' => 'Ubuntu',
-            '/iphone/i' => 'iPhone',
-            '/ipod/i' => 'iPod',
-            '/ipad/i' => 'iPad',
-            '/android/i' => 'Android',
-            '/blackberry/i' => 'BlackBerry',
-            '/webos/i' => 'Mobile'
-        );
-
-        foreach ($testArray as $regex => $value) {
-            if (preg_match($regex, $userAgent)) {
-                return $value;
-            }
-        }
-
-        // Return default:
-        return "Unknown OS Platform";
-    }
-
-    /**
      * Get current browser name
      * @return string
      */
