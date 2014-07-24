fb8f30d257e33ba5362079678b6dda90112e93e8
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-07-24 13:37:19 +0000

    GLICES-175 : Allow to detect MAC/Windows OS

--- trunk/http/header/request.php
+++ trunk/http/header/request.php
@@ -5,91 +5,118 @@ use Mu\Kernel;
 
 class Request
 {
-	/**
-	 * @return string
-	 */
-	public function getAccept()
-	{
-		return isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '*/*';
-	}
+    const OS_OTHER = 1;
+    const OS_WINDOWS = 2;
+    const OS_MAC = 3;
 
-	/**
-	 * @return string
-	 */
-	public function getAcceptEncoding()
-	{
-		return isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '';
-	}
+    /**
+     * @return string
+     */
+    public function getAccept()
+    {
+        return isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '*/*';
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getAcceptLanguage()
-	{
-		$acceptedLanguage = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
-		if (!strlen($acceptedLanguage)) {
-			return array();
-		}
+    /**
+     * @return string
+     */
+    public function getAcceptEncoding()
+    {
+        return isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '';
+    }
 
-		$acceptedLanguage = explode(',', $acceptedLanguage);
-		$finalAcceptedLanguage = array();
-		foreach ($acceptedLanguage as $oneLang) {
-			$oneLang = explode(';', $oneLang);
-			$arrayLen = count($oneLang);
+    /**
+     * @return array
+     */
+    public function getAcceptLanguage()
+    {
+        $acceptedLanguage = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+        if (!strlen($acceptedLanguage)) {
+            return array();
+        }
 
-			if ($arrayLen == 1) {
-				$strLang = reset($oneLang);
-				$quality = 1;
-			} elseif ($arrayLen == 2) {
-				$strLang = reset($oneLang);
-				$quality = (float)str_replace('q=', '', next($oneLang));
-			} else {
-				continue;
-			}
+        $acceptedLanguage = explode(',', $acceptedLanguage);
+        $finalAcceptedLanguage = array();
+        foreach ($acceptedLanguage as $oneLang) {
+            $oneLang = explode(';', $oneLang);
+            $arrayLen = count($oneLang);
 
-			$finalAcceptedLanguage[$strLang] = $quality * 100;
-		}
+            if ($arrayLen == 1) {
+                $strLang = reset($oneLang);
+                $quality = 1;
+            } elseif ($arrayLen == 2) {
+                $strLang = reset($oneLang);
+                $quality = (float)str_replace('q=', '', next($oneLang));
+            } else {
+                continue;
+            }
 
-		return $finalAcceptedLanguage;
-	}
+            $finalAcceptedLanguage[$strLang] = $quality * 100;
+        }
 
-	/**
-	 * @return string
-	 */
-	public function getConnection()
-	{
-		return isset($_SERVER['HTTP_CONNECTION']) ? $_SERVER['HTTP_CONNECTION'] : '';
-	}
+        return $finalAcceptedLanguage;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getHost()
-	{
-		return isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
-	}
+    /**
+     * @return string
+     */
+    public function getConnection()
+    {
+        return isset($_SERVER['HTTP_CONNECTION']) ? $_SERVER['HTTP_CONNECTION'] : '';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getReferer()
-	{
-		return isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-	}
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getUserAgent()
-	{
-		return isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-	}
+    /**
+     * @return string
+     */
+    public function getReferer()
+    {
+        return isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getHttps()
-	{
-		return isset($_SERVER['HTTP_HTTPS']) ? $_SERVER['HTTP_HTTPS'] : false;
-	}
+    /**
+     * @return string
+     */
+    public function getUserAgent()
+    {
+        return isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getHttps()
+    {
+        return isset($_SERVER['HTTP_HTTPS']) ? $_SERVER['HTTP_HTTPS'] : false;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOS()
+    {
+        $ua = $this->getUserAgent();
+        if (preg_match('#windows#i', $ua)
+            || preg_match('#cygwin_nt#i', $ua)
+            || preg_match('#os\/2#i', $ua)
+        ) {
+            return self::OS_WINDOWS;
+        } else {
+            if (preg_match('#mac\sos#i', $ua)
+                || preg_match('#mac_powerpc#i', $ua)
+                || preg_match('#macintosh#i', $ua)
+            ) {
+                return self::OS_MAC;
+            } else {
+                return self::OS_OTHER;
+            }
+        }
+    }
 }
