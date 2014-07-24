67bfe27c372c16ce2d9bf847ff58ca2e07d410d1
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-07-24 09:35:08 +0000

    MGM-359 : Multilanguage for golf, course and pages

--- trunk/helper/form.php
+++ trunk/helper/form.php
@@ -15,224 +15,239 @@ use Mu\Kernel\Model\Manager;
  */
 class Form extends Kernel\Service\Core
 {
-	const METHOD_POST = 'POST';
-	const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_GET = 'GET';
 
-	const TYPE_DEFAULT  = 'text';
-    const TYPE_INPUT    = 'input';
-	const TYPE_HIDDEN   = 'hidden';
-	const TYPE_PASSWORD = 'password';
-	const TYPE_TEXTAREA = 'textarea';
-	const TYPE_CHECK    = 'checkbox';
-	const TYPE_RADIO    = 'radio';
-	const TYPE_SELECT   = 'select';
-	const TYPE_CBLIST   = 'checkboxlist';
+    const TYPE_DEFAULT = 'text';
+    const TYPE_INPUT = 'input';
+    const TYPE_HIDDEN = 'hidden';
+    const TYPE_PASSWORD = 'password';
+    const TYPE_TEXTAREA = 'textarea';
+    const TYPE_CHECK = 'checkbox';
+    const TYPE_RADIO = 'radio';
+    const TYPE_SELECT = 'select';
+    const TYPE_CBLIST = 'checkboxlist';
 
-	protected $action = '';
-	protected $enctype = 'multipart/form-data';
-	protected $method = self::METHOD_POST;
-	protected $allowedMethod = array(self::METHOD_GET, self::METHOD_POST);
-	protected $allowedTypes = array(
-		self::TYPE_DEFAULT,
-		self::TYPE_HIDDEN,
-		self::TYPE_PASSWORD,
-		self::TYPE_TEXTAREA,
-		self::TYPE_CHECK,
-		self::TYPE_RADIO,
-		self::TYPE_SELECT,
-		self::TYPE_CBLIST
-	);
-	protected $defaultField = array(
-		'type' => self::TYPE_DEFAULT,
-		'value' => null,
-		'default' => '', // Default value
-		'class' => '', // Specific field class
-		'required' => false, // Is field required
-		'pattern' => '', // Pattern to add to input fields (HTML5)
-		'values' => [], // Values for checkbox, radio and select
-		'multiple' => false, // Multiple select declarator
-		'separator' => ',', // Default separator (for DB storage)
-	);
+    protected $action = '';
+    protected $enctype = 'multipart/form-data';
+    protected $method = self::METHOD_POST;
+    protected $allowedMethod = array(self::METHOD_GET, self::METHOD_POST);
+    protected $allowedTypes = array(
+        self::TYPE_DEFAULT,
+        self::TYPE_HIDDEN,
+        self::TYPE_PASSWORD,
+        self::TYPE_TEXTAREA,
+        self::TYPE_CHECK,
+        self::TYPE_RADIO,
+        self::TYPE_SELECT,
+        self::TYPE_CBLIST
+    );
+    protected $defaultField = array(
+        'type' => self::TYPE_DEFAULT,
+        'value' => null,
+        'default' => '', // Default value
+        'class' => '', // Specific field class
+        'required' => false, // Is field required
+        'pattern' => '', // Pattern to add to input fields (HTML5)
+        'values' => [], // Values for checkbox, radio and select
+        'multiple' => false, // Multiple select declarator
+        'separator' => ',', // Default separator (for DB storage)
+    );
 
-	/**
-	 * @param mixed $submit
-	 * @param string $class
-	 * @return array
-	 */
-	public function getFormInfos($submit = null, $class = '')
-	{
-		return array(
-			'action' => $this->action,
-			'method' => $this->method,
-			'enctype' => $this->enctype,
-			'class' => $class,
-			'submit' => $submit,
-			'fieldAfter' => '<br />',
-			'fieldBefore' => '',
-		);
-	}
+    /**
+     * @param mixed $submit
+     * @param string $class
+     * @return array
+     */
+    public function getFormInfos($submit = null, $class = '')
+    {
+        return array(
+            'action' => $this->action,
+            'method' => $this->method,
+            'enctype' => $this->enctype,
+            'class' => $class,
+            'submit' => $submit,
+            'fieldAfter' => '<br />',
+            'fieldBefore' => '',
+        );
+    }
 
-	/**
-	 * Generate the formated array containing data for the form rendering
-	 * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
-	 * @param array $properties
-	 * @param null $group
-	 * @return array
-	 */
-	public function getFields($object, array $properties = array(), $group = null)
-	{
-		// Get entity and manager:
-		if (is_a($object, '\\Mu\\Kernel\\Model\\Entity')) {
-			$manager = $object->getManager();
-			/** @var Entity $entity */
-			$entity = $object;
-		} else {
-			/** @var Manager $manager */
-			$manager = $object;
-			$entity = null;
-		}
+    /**
+     * Generate the formated array containing data for the form rendering
+     * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
+     * @param array $properties
+     * @param string $group
+     * @param string $lang
+     * @return array
+     */
+    public function getFields($object, array $properties = array(), $group = null, $lang = null)
+    {
+        // Get entity and manager:
+        if (is_a($object, '\\Mu\\Kernel\\Model\\Entity')) {
+            $manager = $object->getManager();
+            /** @var Entity $entity */
+            $entity = $object;
+        } else {
+            /** @var Manager $manager */
+            $manager = $object;
+            $entity = null;
+        }
 
-		// Get manager key:
-		$allProperties = $manager->getProperties();
-		if (!isset($allProperties[$group])) {
-			$group = $manager->getDefaultGroup();
-		}
+        // Get manager key:
+        $allProperties = $manager->getProperties();
+        if (!isset($allProperties[$group])) {
+            $group = $manager->getDefaultGroup();
+        }
 
-		// Get manager properties or return array:
-		if (!is_array($allProperties[$group]['properties'])) {
-			return [];
-		}
+        // Get manager properties or return array:
+        if (!is_array($allProperties[$group]['properties'])) {
+            return [];
+        }
 
-		// Get properties to check:
-		$properties = (!empty($properties)) ? array_intersect_key(
-			$allProperties[$group]['properties'],
-			array_flip($properties)
-		)
-			: $allProperties[$group]['properties'];
+        // Get properties to check:
+        $properties = (!empty($properties)) ? array_intersect_key(
+            $allProperties[$group]['properties'],
+            array_flip($properties)
+        )
+            : $allProperties[$group]['properties'];
 
-		// Start form creation:
-		$form = [];
-		foreach ($properties as $id => $values) {
-			if (!is_array($values['form'])) {
-				continue;
-			}
+        // Start form creation:
+        $form = [];
+        foreach ($properties as $id => $values) {
+            if (!is_array($values['form'])) {
+                continue;
+            }
 
-			// Set generalities:
-			$field = array_merge($this->defaultField, $values['form']);
-			$field['label'] = (isset($values['title'])) ? $values['title'] : null;
-			$field['length'] = (isset($values['length'])) ? $values['length'] : null;
+            // Set generalities:
+            $field = array_merge($this->defaultField, $values['form']);
+            $field['label'] = (isset($values['title'])) ? $values['title'] : null;
+            $field['length'] = (isset($values['length'])) ? $values['length'] : null;
 
-			// Check input type:
-			if (!isset($field['type']) || $field['type'] == self::TYPE_INPUT) {
-				$field['type'] = self::TYPE_DEFAULT;
-			}
+            // Check input type:
+            if (!isset($field['type']) || $field['type'] == self::TYPE_INPUT) {
+                $field['type'] = self::TYPE_DEFAULT;
+            }
 
-			// Set default values for radio and select:
-			if (in_array($field['type'], [self::TYPE_RADIO, self::TYPE_SELECT, self::TYPE_CBLIST]) && !is_array($field['values'])) {
-				if (method_exists($manager, $field['values'])) {
-					$field['values'] = $manager->$field['values']();
-				} else {
-					continue;
-				}
-			}
+            // Set default values for radio and select:
+            if (in_array($field['type'], [self::TYPE_RADIO, self::TYPE_SELECT, self::TYPE_CBLIST]) && !is_array(
+                    $field['values']
+                )
+            ) {
+                if (method_exists($manager, $field['values'])) {
+                    $field['values'] = $manager->$field['values']();
+                } else {
+                    continue;
+                }
+            }
 
-			// Add value if entity is found and its method too:
-			$getter = 'get' . ucfirst($id);
-			if ($entity != null and method_exists($entity, $getter)) {
-				$value = $entity->$getter();
-				if (!empty($value)) {
-					$field['value'] = $value;
-				}
-			}
+            // Add value if entity is found and its method too:
+            $value = null;
+            if ($lang == null) {
+                $getter = 'get' . ucfirst($id);
+                if ($entity != null and method_exists($entity, $getter)) {
+                    $value = $entity->$getter();
+                }
+            } else {
+                $value = $object->getLocalizedValue($id, $lang);
+            }
 
-			// Add default input name:
-			if (!isset($field['name'])) {
-				$field['name'] = $id;
-			}
+            if (!empty($value)) {
+                $field['value'] = $value;
+            }
 
-			// Add default field ID:
-			if (!isset($field['id'])) {
-				$field['id'] = $id;
-			}
+            // Add default input name:
+            if (!isset($field['name'])) {
+                $field['name'] = $id;
+            }
 
-			// Add field into the form array:
-			$form[$id] = $field;
-		}
+            // Add default field ID:
+            if (!isset($field['id'])) {
+                $field['id'] = $id;
+            }
 
-		// Return formated array:
-		return $form;
-	}
+            if ($lang != null) {
+                $field['id'] .= '_' . $lang;
+            }
 
-	/**
-	 * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
-	 * @param $field
-	 * @param null $group
-	 * @return array|null
-	 */
-	public function getField($object, $field, $group = null)
-	{
-		$return = reset($this->getFields($object, [$field], $group));
-		return $return;
-	}
+            // Add field into the form array:
+            $form[$id] = $field;
+        }
 
-	/**
-	 * Test if all requeried fields are set
-	 * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
-	 * @param array $properties
-	 * @return bool
-	 */
-	public function isValid($object, array $properties = array())
-	{
-		// Get manager fields:
-		$fields = $this->getForm($object, $properties);
+        // Return formated array:
+        return $form;
+    }
 
-		// Get request:
-		$request = $this->getApp()->getHttp()->getRequest();
+    /**
+     * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
+     * @param $field
+     * @param string $group
+     * @param string $lang
+     * @return array|null
+     */
+    public function getField($object, $field, $group = null, $lang = null)
+    {
+        $return = reset($this->getFields($object, [$field], $group, $lang));
+        return $return;
+    }
 
-		// Test fields:
-		foreach ($fields as $id => $field) {
-			if (!$field['required']) {
-				continue;
-			}
+    /**
+     * Test if all requeried fields are set
+     * @param \Mu\Kernel\Model\Entity|\Mu\Kernel\Model\Manager $object
+     * @param array $properties
+     * @return bool
+     */
+    public function isValid($object, array $properties = array())
+    {
+        // Get manager fields:
+        $fields = $this->getForm($object, $properties);
 
-			// Get value from POST:
-			$value = $request->post($id, null);
-			if ($value === null) {
-				return false;
-			}
-		}
+        // Get request:
+        $request = $this->getApp()->getHttp()->getRequest();
 
-		// Return true:
-		return true;
-	}
+        // Test fields:
+        foreach ($fields as $id => $field) {
+            if (!$field['required']) {
+                continue;
+            }
 
-	/**
-	 * Set action of the form
-	 * @param $route
-	 * @param array $parameters
-	 */
-	public function setAction($route, array $parameters = array())
-	{
-		$this->action = $this->getApp()->getRouteManager()->getUrl($route, $parameters);
-	}
+            // Get value from POST:
+            $value = $request->post($id, null);
+            if ($value === null) {
+                return false;
+            }
+        }
 
-	/**
-	 * Set submit method
-	 * @param string $method
-	 */
-	public function setMethod($method = self::METHOD_POST)
-	{
-		if (in_array($method, $this->allowedMethod)) {
-			$this->method = $method;
-		}
-	}
+        // Return true:
+        return true;
+    }
 
-	/**
-	 * Set encrypt type
-	 * @param $type
-	 */
-	public function setEnctype($type)
-	{
-		$this->enctype = $type;
-	}
+    /**
+     * Set action of the form
+     * @param $route
+     * @param array $parameters
+     */
+    public function setAction($route, array $parameters = array())
+    {
+        $this->action = $this->getApp()->getRouteManager()->getUrl($route, $parameters);
+    }
+
+    /**
+     * Set submit method
+     * @param string $method
+     */
+    public function setMethod($method = self::METHOD_POST)
+    {
+        if (in_array($method, $this->allowedMethod)) {
+            $this->method = $method;
+        }
+    }
+
+    /**
+     * Set encrypt type
+     * @param $type
+     */
+    public function setEnctype($type)
+    {
+        $this->enctype = $type;
+    }
 }
