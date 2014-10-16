53e65771199e56eae4710acf735db9742a9bb579
Schiano Romain <rschiano@users.noreply.github.com>
2014-10-16 08:46:18 +0000

    FWK-152 : Improve database query construction

--- db/interfaces/requestable.php
+++ db/interfaces/requestable.php
@@ -5,65 +5,71 @@ use Mu\Kernel;
 
 interface Requestable
 {
-	/**
-	 * @return array
-	 */
-	public function getProperties();
+    /**
+     * @return array
+     */
+    public function getProperties();
 
-	/**
-	 * @param string $stdOut
-	 * @return bool
-	 */
-	public function createStructure($stdOut = '\print');
+    /**
+     * @param bool $default
+     * @return array
+     */
+    public function getRequestReplaceList($default = false);
 
-	/**
-	 * @param string $stdOut
-	 * @return bool
-	 */
-	public function createDefaultDataSet($stdOut = '\print');
+    /**
+     * @param string $stdOut
+     * @return bool
+     */
+    public function createStructure($stdOut = '\print');
 
-	/**
-	 * @return string
-	 */
-	public function getDefaultGroup();
+    /**
+     * @param string $stdOut
+     * @return bool
+     */
+    public function createDefaultDataSet($stdOut = '\print');
 
-	/**
-	 * @param string $group
-	 * @param string $name
-	 * @throws Kernel\Db\Exception
-	 * @return array
-	 */
-	public function getProperty($group, $name);
+    /**
+     * @return string
+     */
+    public function getDefaultGroup();
 
-	/**
-	 * @param string $group
-	 * @return array
-	 * @throws Kernel\Db\Exception
-	 */
-	public function getGroupPropertyInfos($group);
+    /**
+     * @param string $group
+     * @param string $name
+     * @throws Kernel\Db\Exception
+     * @return array
+     */
+    public function getProperty($group, $name);
 
-	/**
-	 * @param string $group
-	 * @param string $name
-	 * @param bool $isShortMode
-	 * @return string
-	 */
-	public function getPropertyForDb($group, $name, $isShortMode = false);
+    /**
+     * @param string $group
+     * @return array
+     * @throws Kernel\Db\Exception
+     */
+    public function getGroupPropertyInfos($group);
 
-	/**
-	 * @param string $group
-	 * @return string
-	 */
-	public function getGroupForDb($group);
+    /**
+     * @param string $group
+     * @param string $name
+     * @param bool $isShortMode
+     * @return string
+     */
+    public function getPropertyForDb($group, $name, $isShortMode = false);
 
-	/**
-	 * @return Kernel\Db\Handler
-	 * @throws Kernel\Db\Exception
-	 */
-	public function getDbHandler();
+    /**
+     * @param string $group
+     * @return string
+     */
+    public function getGroupForDb($group);
 
-	/**
-	 * @param Kernel\Db\Handler $handler
-	 */
-	public function setDbHandler(Kernel\Db\Handler $handler);
+    /**
+     * @return Kernel\Db\Handler
+     * @throws Kernel\Db\Exception
+     */
+    public function getDbHandler();
+
+    /**
+     * @param Kernel\Db\Handler $handler
+     */
+    public function setDbHandler(Kernel\Db\Handler $handler);
 }
--- db/query.php
+++ db/query.php
@@ -9,142 +9,134 @@ use Mu\Kernel;
 class Query extends Kernel\Core
 {
 
-	protected $values = array();
-	protected $query = array();
-	protected $defaultRequestable;
-	protected $isShortMode = false;
-	protected $comment;
-	protected $type;
+    protected $values = array();
+    protected $query = array();
+    protected $requestableList = array();
+    protected $isShortMode = false;
+    protected $type;
 
-	const TYPE_SELECT = 1;
-	const TYPE_UPDATE = 2;
-	const TYPE_INSERT = 3;
-	const TYPE_UNKNOWN = 4;
+    const TYPE_SELECT = 1;
+    const TYPE_UPDATE = 2;
+    const TYPE_INSERT = 3;
+    const TYPE_UNKNOWN = 4;
 
-	/**
-	 * @param string $query
-	 * @param array $values
-	 * @param Kernel\Db\Interfaces\Requestable $defaultRequestable
-	 * @return Query
-	 */
-	public function __construct(
-		$query,
-		array $values = array(),
-		Kernel\Db\Interfaces\Requestable $defaultRequestable = null
-	) {
-		$this->setValues($values);
-		$this->setQuery($query);
-		$this->setDefaultRequestable($defaultRequestable);
+    /**
+     * @param string $query
+     * @param array $values
+     * @param array|Kernel\Db\Interfaces\Requestable $requestableList
+     * @return Query
+     */
+    public function __construct(
+        $query,
+        array $values = array(),
+        $requestableList = null
+    ) {
+        $this->setValues($values);
+        $this->setQuery($query);
+        $this->setRequestableList($requestableList);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getValues()
-	{
-		return $this->values;
-	}
+    /**
+     * @return array
+     */
+    public function getValues()
+    {
+        return $this->values;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getQuery()
-	{
-		return $this->query;
-	}
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getComment()
-	{
-		return $this->comment;
-	}
+    /**
+     * @return Kernel\Db\Interfaces\Requestable[]
+     */
+    public function getRequestableList()
+    {
+        return $this->requestableList;
+    }
 
-	/**
-	 * @return Kernel\Db\Interfaces\Requestable
-	 */
-	public function getDefaultRequestable()
-	{
-		return $this->defaultRequestable;
-	}
+    /**
+     * @return int
+     */
+    public function getType()
+    {
+        if (!$this->type) {
+            $strType = substr($this->getQuery(), 0, 6);
 
-	/**
-	 * @return int
-	 */
-	public function getType()
-	{
-		if (!$this->type) {
-			$strType = substr($this->getQuery(), 0, 6);
+            switch ($strType) {
+                case 'SELECT':
+                    $this->type = self::TYPE_SELECT;
+                    break;
+                case 'UPDATE':
+                    $this->type = self::TYPE_UPDATE;
+                    break;
+                case 'INSERT':
+                case 'REPLAC':
+                    $this->type = self::TYPE_INSERT;
+                    break;
+                default:
+                    $this->type = self::TYPE_UNKNOWN;
+                    break;
+            }
+        }
 
-			switch ($strType) {
-				case 'SELECT':
-					$this->type = self::TYPE_SELECT;
-					break;
-				case 'UPDATE':
-					$this->type = self::TYPE_UPDATE;
-					break;
-				case 'INSERT':
-				case 'REPLAC':
-					$this->type = self::TYPE_INSERT;
-					break;
-				default:
-					$this->type = self::TYPE_UNKNOWN;
-					break;
-			}
-		}
+        return $this->type;
+    }
 
-		return $this->type;
-	}
+    /**
+     * @return bool
+     */
+    public function isShortMode()
+    {
+        return $this->isShortMode;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isShortMode()
-	{
-		return $this->isShortMode;
-	}
+    /**
+     * @param string $query
+     */
+    public function setQuery($query)
+    {
+        $this->query = trim($query);
+    }
 
-	/**
-	 * @param string $comment
-	 */
-	public function setComment($comment)
-	{
-		$this->comment = $comment;
-	}
+    /**
+     * @param string[] $values
+     * @throws Exception
+     */
+    public function setValues(array $values)
+    {
+        $this->values = $values;
+    }
 
-	/**
-	 * @param string $query
-	 */
-	public function setQuery($query)
-	{
-		$this->query = trim($query);
-	}
+    /**
+     * @param bool $value
+     */
+    public function setShortMode($value)
+    {
+        $this->isShortMode = (bool)$value;
+    }
 
-	/**
-	 * @param string[] $values
-	 * @throws Exception
-	 */
-	public function setValues(array $values)
-	{
-		$this->values = $values;
-	}
-
-	/**
-	 * @param bool $value
-	 */
-	public function setShortMode($value)
-	{
-		$this->isShortMode = (bool)$value;
-	}
-
-	/**
-	 * @param Kernel\Db\Interfaces\Requestable $requestable
-	 */
-	public function setDefaultRequestable(Kernel\Db\Interfaces\Requestable $requestable = null)
-	{
-		$this->defaultRequestable = $requestable;
-	}
+    /**
+     * @param array $requestableList
+     * @throws Exception
+     */
+    public function setRequestableList($requestableList = array())
+    {
+        if (is_array($requestableList)) {
+            $this->requestableList = $requestableList;
+        } else {
+            if ($requestableList instanceof Kernel\Db\Interfaces\Requestable) {
+                $this->requestableList['default'] = $requestableList;
+            } else {
+                throw new Exception(Exception::INVALID_MANAGER, $requestableList);
+            }
+        }
+    }
 }
