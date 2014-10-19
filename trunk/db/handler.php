35f960d6e45c4305fab9b21b27a14ae21ad17e40
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-10-19 16:39:37 +0000

    Fix register

--- trunk/db/handler.php
+++ trunk/db/handler.php
@@ -5,72 +5,75 @@ use Mu\Kernel;
 
 abstract class Handler extends Kernel\Core
 {
-	const PARAM_INT = 1;
-	const PARAM_STR = 2;
-	const PARAM_BOOL = 3;
-	const PARAM_FLOAT = 4;
+    const PARAM_INT = 1;
+    const PARAM_STR = 2;
+    const PARAM_BOOL = 3;
+    const PARAM_FLOAT = 4;
 
-	protected $knowActions = array(
-		'tinyint' => self::PARAM_INT,
-		'smallint' => self::PARAM_INT,
-		'mediumint' => self::PARAM_INT,
-		'int' => self::PARAM_INT,
-		'bigint' => self::PARAM_INT,
-		'timestamp' => self::PARAM_STR,
-		'float' => self::PARAM_FLOAT,
-		'bool' => self::PARAM_BOOL,
-		'string' => self::PARAM_STR,
-		'varchar' => self::PARAM_STR,
-		'char' => self::PARAM_STR,
-		'blob' => self::PARAM_STR,
-		'text' => self::PARAM_STR,
-		'long_blob' => self::PARAM_STR,
-		'long_text' => self::PARAM_STR,
-		'date' => self::PARAM_STR,
-		'ip' => self::PARAM_INT,
-	);
-	protected $typeCheckValues = array(
-		Query::TYPE_SELECT,
-		Query::TYPE_UPDATE,
-	);
+    protected $knowActions = array(
+        'tinyint' => self::PARAM_INT,
+        'smallint' => self::PARAM_INT,
+        'mediumint' => self::PARAM_INT,
+        'int' => self::PARAM_INT,
+        'bigint' => self::PARAM_INT,
+        'timestamp' => self::PARAM_STR,
+        'float' => self::PARAM_FLOAT,
+        'bool' => self::PARAM_BOOL,
+        'string' => self::PARAM_STR,
+        'varchar' => self::PARAM_STR,
+        'char' => self::PARAM_STR,
+        'blob' => self::PARAM_STR,
+        'text' => self::PARAM_STR,
+        'long_blob' => self::PARAM_STR,
+        'long_text' => self::PARAM_STR,
+        'date' => self::PARAM_STR,
+        'ip' => self::PARAM_INT,
+    );
+    protected $typeCheckValues = array(
+        Query::TYPE_SELECT,
+        Query::TYPE_UPDATE,
+    );
 
     protected $cacheQuery = array();
     protected $link;
 
-	/**
-	 * @return mixed
-	 */
-	public function getLink()
-	{
-		return $this->link;
-	}
+    /**
+     * @return mixed
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
 
-	/**
-	 * @param mixed $link
-	 */
-	public function setLink($link)
-	{
+    /**
+     * @param mixed $link
+     */
+    public function setLink($link)
+    {
         $this->link = $link;
     }
 
-	/**
-	 * @param string $string
-	 * @return string
-	 */
-	abstract protected function protectString($string);
+    /**
+     * @param string $string
+     * @return string
+     */
+    abstract protected function protectString($string);
 
-	/**
-	 * Check if query is valid
-	 *
-	 * @param Query $query
-	 * @throws Exception
-	 * @return bool
-	 */
-	protected function checkQuery(Query $query)
-	{
-		$strQuery = $query->getQuery();
+    /**
+     * Check if query is valid
+     *
+     * @param Query $query
+     * @throws Exception
+     * @return bool
+     */
+    protected function checkQuery(Query $query)
+    {
+        $strQuery = $query->getQuery();
         $isShortMode = $query->isShortMode();
-        $hashQuery = sha1($strQuery) . '|' . ((int)$isShortMode);
+        $requestableList = $query->getRequestableList();
+        $defaultRequestable = $requestableList['default'];
+
+        $hashQuery = md5(get_class($defaultRequestable)) . '|' . sha1($strQuery) . '|' . ((int)$isShortMode);
         $checkValue = in_array($query->getType(), $this->typeCheckValues);
 
         #region value check analysis
@@ -78,8 +81,6 @@ abstract class Handler extends Kernel\Core
         $valuesOffset = count($values) - 1;
         $lastFound = strrpos($strQuery, '?');
         $subQuery = $strQuery;
-        $requestableList = $query->getRequestableList();
-        $defaultRequestable = $requestableList['default'];
         $defaultGroup = $defaultRequestable->getDefaultGroup();
 
         while ($lastFound !== false) {
@@ -204,108 +205,108 @@ abstract class Handler extends Kernel\Core
     }
 
     /**
-	 * @param mixed $value
-	 * @param int $type
-	 * @return int|string
-	 */
-	private function checkProperty($value, $type)
-	{
-		switch ($type) {
-			case self::PARAM_INT:
-				return (int)$value;
-				break;
-			case self::PARAM_STR:
-				return $this->protectString($value);
-				break;
-			case self::PARAM_BOOL:
-				return (int)(bool)$value;
-				break;
-			case self::PARAM_FLOAT:
-				return (float)$value;
-				break;
-			default:
-				return $value;
-				break;
-		}
-	}
+     * @param mixed $value
+     * @param int $type
+     * @return int|string
+     */
+    private function checkProperty($value, $type)
+    {
+        switch ($type) {
+            case self::PARAM_INT:
+                return (int)$value;
+                break;
+            case self::PARAM_STR:
+                return $this->protectString($value);
+                break;
+            case self::PARAM_BOOL:
+                return (int)(bool)$value;
+                break;
+            case self::PARAM_FLOAT:
+                return (float)$value;
+                break;
+            default:
+                return $value;
+                break;
+        }
+    }
 
-	/**
-	 * @param array $property
-	 * @return Kernel\Db\Traits\Requestable
-	 * @throws Exception
-	 */
-	private function getManager(array $property)
-	{
-		if (!isset($property['manager'])) {
-			throw new Exception(print_r($property, true), Exception::INVALID_MANAGER);
-		}
-		return $this->getApp()->getModelManager()->getOneManager($property['manager']);
-	}
+    /**
+     * @param array $property
+     * @return Kernel\Db\Traits\Requestable
+     * @throws Exception
+     */
+    private function getManager(array $property)
+    {
+        if (!isset($property['manager'])) {
+            throw new Exception(print_r($property, true), Exception::INVALID_MANAGER);
+        }
+        return $this->getApp()->getModelManager()->getOneManager($property['manager']);
+    }
 
-	/**
-	 * @return bool
-	 */
-	public abstract function startTransaction();
+    /**
+     * @return bool
+     */
+    public abstract function startTransaction();
 
-	/**
-	 * @return bool
-	 */
-	public abstract function commitTransaction();
+    /**
+     * @return bool
+     */
+    public abstract function commitTransaction();
 
-	/**
-	 * @return bool
-	 */
-	public abstract function rollbackTransaction();
+    /**
+     * @return bool
+     */
+    public abstract function rollbackTransaction();
 
-	/**
-	 * @param string $query
-	 * @return Result
-	 */
-	public abstract function query($query);
+    /**
+     * @param string $query
+     * @return Result
+     */
+    public abstract function query($query);
 
-	/**
-	 * @param Query $query
-	 * @return Result
-	 */
-	public abstract function sendQuery(Kernel\Db\Query $query);
+    /**
+     * @param Query $query
+     * @return Result
+     */
+    public abstract function sendQuery(Kernel\Db\Query $query);
 
-	/**
-	 * @return bool
-	 */
-	public abstract function hasError();
+    /**
+     * @return bool
+     */
+    public abstract function hasError();
 
-	/**
-	 * @return string
-	 */
-	public abstract function getErrors();
+    /**
+     * @return string
+     */
+    public abstract function getErrors();
 
-	/**
-	 * @return string
-	 */
-	public abstract function getLastError();
+    /**
+     * @return string
+     */
+    public abstract function getLastError();
 
-	/**
-	 * @return int
-	 */
-	public abstract function getInsertId();
+    /**
+     * @return int
+     */
+    public abstract function getInsertId();
 
-	/**
-	 * @param array $property
-	 * @return string
-	 */
-	public abstract function getStructureFromProperty(array $property);
+    /**
+     * @param array $property
+     * @return string
+     */
+    public abstract function getStructureFromProperty(array $property);
 
-	/**
-	 * @param string $tableToken
-	 * @param string $keyName
-	 * @param array $key
-	 * @return string
-	 */
-	public abstract function getStructureFromKey($tableToken, $keyName, array $key);
+    /**
+     * @param string $tableToken
+     * @param string $keyName
+     * @param array $key
+     * @return string
+     */
+    public abstract function getStructureFromKey($tableToken, $keyName, array $key);
 
-	/**
-	 * @param array $tableInfos
-	 * @return string
-	 */
-	public abstract function getStructureFromTableInfos(array $tableInfos);
+    /**
+     * @param array $tableInfos
+     * @return string
+     */
+    public abstract function getStructureFromTableInfos(array $tableInfos);
 }
