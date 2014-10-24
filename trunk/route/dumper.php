cba0310a7591022a0c398d288eec0fbe25de001e
rschiano <rschiano@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-10-24 14:11:25 +0000

    Fix nginx rules dump

--- trunk/route/dumper.php
+++ trunk/route/dumper.php
@@ -5,45 +5,42 @@ use Mu\Kernel;
 
 abstract class Dumper extends Kernel\Core
 {
-	/**
-	 * @param Route[] $routes
-	 */
-	abstract public function dumpRoutes($routes);
+    /**
+     * @param Route[] $routes
+     * @return
+     */
+    abstract protected function dumpRoutes($routes);
 
-	/**
-	 * @param Route $route
-	 * @return array
-	 */
-	public function prepareRuleVars(Route $route)
-	{
-		$pattern = $route->getPattern();
-		if (strlen($pattern) && $pattern{0} == '/') {
-			$pattern = substr($pattern, 1);
-		}
-		$pattern = str_replace('/', '\/', '^' . $pattern . '\.?('.implode('|',$route->getAllowedFormats()).')?$');
-		$vars = array(
-			'rn=' . $route->getName()
-		);
+    abstract public function dumpSites($sites);
 
-		$defaultVars = $route->getDefaultVars();
-		while (preg_match('#\{([^\}\/]+)}#iu', $pattern, $matches)) {
-			$match = $matches[1];
+    /**
+     * @param Kernel\Route\Route $route
+     * @return array
+     */
+    public function prepareRuleVars(Kernel\Route\Route $route)
+    {
+        $pattern = $route->getPattern();
+        $pattern = str_replace('/', '\/', '^' . $pattern . '$');
+        $vars = array(
+            'rn=' . $route->getName()
+        );
 
-			if (isset($defaultVars[$match])) {
-				$pattern = str_replace('{' . $match . '}\/', '([^\/\.]+)?\/?', $pattern);
-			}
-			$pattern = str_replace('{' . $match . '}', '([^\/\.]+)', $pattern);
-			$vars[] = $match . '=$' . count($vars);
-		}
+        $defaultVars = $route->getDefaultVars();
+        while (preg_match('#\{([^\}\/]+)}#iu', $pattern, $matches)) {
+            $match = $matches[1];
 
-		// Add format var:
-		$vars[] = 'format=$' . count($vars);
+            if (isset($defaultVars[$match])) {
+                $pattern = str_replace('{' . $match . '}\/', '([^\/\.]+)?\/?', $pattern);
+            }
+            $pattern = str_replace('{' . $match . '}', '([^\/\.]+)', $pattern);
+            $vars[] = $match . '=$' . count($vars);
+        }
 
-		$dest = 'index.php?' . implode('&', $vars);
+        $dest = '/index.php?' . implode('&', $vars);
 
-		return array(
-			'dest' => $dest,
-			'pattern' => $pattern
-		);
-	}
+        return array(
+            'dest' => $dest,
+            'pattern' => $pattern
+        );
+    }
 }
