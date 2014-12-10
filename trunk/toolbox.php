9f7fed30596bd3dd4a6957fe7d0a1a498c88bb6d
azampieri <azampieri@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-12-10 14:15:28 +0000

    js on contact finished. error corrected in cart add function isValidEmail on toolbox.

--- trunk/toolbox.php
+++ trunk/toolbox.php
@@ -909,6 +909,15 @@ class Toolbox extends Service\Core
 
         return $entities;
     }
+
+	public function isValidEmail($email){
+		if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email))
+			return true;
+		else
+			return false;
+	}
+
+
     #endregion
 
     /************************************************************************************
