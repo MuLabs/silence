0b7db0a031d01913d0bdce8373de6fc70defc938
ostahl <ostahl@5f25d12c-12da-4f71-acf6-2e683ce1fd13>
2014-12-12 10:04:48 +0000

    Fix jsonSerialize issue

--- trunk/renderer/handler/json.php
+++ trunk/renderer/handler/json.php
@@ -10,13 +10,7 @@ class Json extends Kernel\Renderer\Handler
      */
 	public function render(Kernel\View\View $view)
 	{
-        $vars = $view->getSafeVars();
-        foreach ($vars as $key => $oneVar) {
-            if ($oneVar instanceof Kernel\Core) {
-                $vars[$key]->setApp(null);
-            }
-        }
-		return json_encode($vars);
+		return json_encode($view->getSafeVars());
 	}
 
     /**
