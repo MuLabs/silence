5ef42b7c650bac5fe307ae94e4c287b0a392325c
Olivier Stahl <cortex2001@gmail.com>
2015-08-02 19:25:22 +0000

    Application installation : use only one context for installations and updates (CONTEXT_SYSTEM => sys)

--- application.php
+++ application.php
@@ -883,19 +883,17 @@ abstract class Application
             return true;
         }
 
-        // Try to connect to the default DB, else create it:
-        try {
-            $handler = $dbManager->getHandler($this->getDefaultDbContext());
-            $handler->query('DROP DATABASE IF EXISTS `' . $this->getDefaultDatabase() . '`');
-            call_user_func($stdOut, 'Removing database...');
-        } catch(\Exception $e) {
-            $handler = $dbManager->getHandler($dbManager->getConstant('CONTEXT_INSTALL'));
-            call_user_func($stdOut, 'Creating database...');
-        }
+        // Get system handler:
+        $handler = $dbManager->getHandler($dbManager->getConstant('CONTEXT_SYSTEM'));
+
+        // Remove previous DB:
+        call_user_func($stdOut, 'Removing database...');
+        $handler->query('DROP DATABASE IF EXISTS `' . $this->getDefaultDatabase() . '`');
 
         // Then create DB:
         $handler->query('CREATE DATABASE IF NOT EXISTS `sys_empty`');
         $handler->query('USE `sys_empty`');
+        call_user_func($stdOut, 'Creating database...');
         $handler->query('CREATE DATABASE `' . $this->getDefaultDatabase() . '`');
         $handler->query('USE `' . $this->getDefaultDatabase() . '`');
         $handler->query('DROP DATABASE `sys_empty`');
